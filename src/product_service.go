f99a38c501ab846325de69eba4aa1822788817f1
Wang-Kai-Chi <ericwangcatch@gmail.com>
2023-08-22 12:06:36 +0800

    update product_service.go and test

--- src/product_service.go
+++ src/product_service.go
@@ -1,6 +1,12 @@
 package main
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func (p Product) Query(db *sql.DB, limit int) ([]Product, error) {
 	rows, err := db.Query(
@@ -28,4 +34,41 @@ func (p Product) Query(db *sql.DB, limit int) ([]Product, error) {
 	db.Close()
 
 	return products, err
+}
+
+func (p Product) Insert(db *sql.DB, ps []Product) (int64, error) {
+	sqlStr := "INSERT INTO product(barcode, product_title, publisher, publication_date, price, quantity) VALUES "
+
+	var inserts []string
+	var params []interface{}
+
+	for _, v := range ps {
+		inserts = append(inserts, "($1, $2, $3, $4, $5, $6)")
+
+		price, err := strconv.Atoi(strings.ReplaceAll(v.Price, "元", ""))
+
+		if err == nil {
+			params = append(params, v.Barcode, v.Product_title, v.Publisher, v.PublicationDate, price, v.Quantity)
+		}
+	}
+
+	insertVals := strings.Join(inserts, ",")
+	sqlStr = sqlStr + insertVals
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+
+	stmt, err := db.PrepareContext(ctx, sqlStr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := stmt.ExecContext(ctx, params...)
+	if err != nil {
+		panic(err)
+	}
+	rows, err := res.RowsAffected()
+
+	return rows, err
 }
--- src/product_service_test.go
+++ src/product_service_test.go
@@ -21,3 +21,29 @@ func TestQuery(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestInsert(t *testing.T) {
+	db, err := ConnectDB()
+
+	if err == nil {
+		var p Product
+
+		products := []Product{{
+			Barcode:         "10000000",
+			Product_title:   "testProduct",
+			Publisher:       "testP",
+			PublicationDate: "1995-01-01",
+			Price:           "69元",
+			Quantity:        1,
+		}}
+		rows, err := p.Insert(db, products)
+
+		if err == nil {
+			fmt.Println(rows)
+		} else {
+			t.Fatal(err)
+		}
+	} else {
+		t.Fatal(err)
+	}
+}
